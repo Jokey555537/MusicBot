7d2d865b479676cd6f9dc6ddcfe66b4a708fcde6
Jokey555537 <54215713+Jokey555537@users.noreply.github.com>
2019-08-17 20:01:12 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,66 +1,74 @@
-const Util = require('./util/Util');
+const botconfig = require("./botconfig.json");
+const Discord = require("discord.js");
+const YTDL = require("ytdl-core");
+const PREFIX = ".";
 
-module.exports = {
-  // "Root" classes (starting points)
-  Client: require('./client/Client'),
-  Shard: require('./sharding/Shard'),
-  ShardClientUtil: require('./sharding/ShardClientUtil'),
-  ShardingManager: require('./sharding/ShardingManager'),
-  WebhookClient: require('./client/WebhookClient'),
+var bot = new Discord.Client();
 
-  // Utilities
-  Collection: require('./util/Collection'),
-  Constants: require('./util/Constants'),
-  DiscordAPIError: require('./client/rest/DiscordAPIError'),
-  EvaluatedPermissions: require('./util/Permissions'),
-  Permissions: require('./util/Permissions'),
-  Snowflake: require('./util/Snowflake'),
-  SnowflakeUtil: require('./util/Snowflake'),
-  Util: Util,
-  util: Util,
-  version: require('../package').version,
+function play(connection, message){
+  var server = servers[message.guild.id];
 
-  // Shortcuts to Util methods
-  escapeMarkdown: Util.escapeMarkdown,
-  fetchRecommendedShards: Util.fetchRecommendedShards,
-  splitMessage: Util.splitMessage,
+  server.dispatcher = connection.playStream(YTDL(server.queue[0], {filter: "audioonly"}));
 
-  // Structures
-  Attachment: require('./structures/Attachment'),
-  CategoryChannel: require('./structures/CategoryChannel'),
-  Channel: require('./structures/Channel'),
-  ClientUser: require('./structures/ClientUser'),
-  ClientUserSettings: require('./structures/ClientUserSettings'),
-  Collector: require('./structures/interfaces/Collector'),
-  DMChannel: require('./structures/DMChannel'),
-  Emoji: require('./structures/Emoji'),
-  Game: require('./structures/Presence').Game,
-  GroupDMChannel: require('./structures/GroupDMChannel'),
-  Guild: require('./structures/Guild'),
-  GuildAuditLogs: require('./structures/GuildAuditLogs'),
-  GuildChannel: require('./structures/GuildChannel'),
-  GuildMember: require('./structures/GuildMember'),
-  Invite: require('./structures/Invite'),
-  Message: require('./structures/Message'),
-  MessageAttachment: require('./structures/MessageAttachment'),
-  MessageCollector: require('./structures/MessageCollector'),
-  MessageEmbed: require('./structures/MessageEmbed'),
-  MessageMentions: require('./structures/MessageMentions'),
-  MessageReaction: require('./structures/MessageReaction'),
-  NewsChannel: require('./structures/NewsChannel'),
-  OAuth2Application: require('./structures/OAuth2Application'),
-  ClientOAuth2Application: require('./structures/OAuth2Application'),
-  PartialGuild: require('./structures/PartialGuild'),
-  PartialGuildChannel: require('./structures/PartialGuildChannel'),
-  PermissionOverwrites: require('./structures/PermissionOverwrites'),
-  Presence: require('./structures/Presence').Presence,
-  ReactionEmoji: require('./structures/ReactionEmoji'),
-  ReactionCollector: require('./structures/ReactionCollector'),
-  RichEmbed: require('./structures/RichEmbed'),
-  Role: require('./structures/Role'),
-  StoreChannel: require('./structures/StoreChannel'),
-  TextChannel: require('./structures/TextChannel'),
-  User: require('./structures/User'),
-  VoiceChannel: require('./structures/VoiceChannel'),
-  Webhook: require('./structures/Webhook'),
-};
+  server.queue.shift();
+
+  server.dispatcher.on("end", function(){
+    if(server.queue[0]) play(connection, message);
+    else connection.disconnect();
+  });
+}
+
+var servers = {};
+
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online!`);
+  bot.user.setGame(".play | by Jokey")
+});
+
+bot.on("message", function(message){
+  if(message.author.equals(bot.user)) return;
+
+  if(!message.content.startsWith(PREFIX)) return;
+
+  var args = message.content.substring(PREFIX.length).split(" ");
+
+switch (args[0].toLowerCase()) {
+  case "play" :
+    if(!args[1]){
+      message.channel.sendMessage("Bitte gib einen Link an");
+      return;
+    }
+
+    if(!message.member.voiceChannel){
+      message.channel.sendMessage("Du musst in einem VoiceChannel sein!");
+      return;
+    }
+
+    if(!servers[message.guild.id]) servers[message.guild.id] = {
+        queue: []
+    };
+
+    var server = servers[message.guild.id];
+
+    server.queue.push(args[1]);
+
+    if(!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection) {
+      play(connection, message);
+    });
+    break;
+  case "skip" :
+    var server = servers[message.guild.id];
+
+    if(server.dispatcher) server.dispatcher.end();
+  break;
+case "stop" :
+  var server = servers[message.guild.id];
+
+  if(message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
+  break;
+default:
+  message.channel.sendMessage("Invalid command");
+    }
+});
+
+bot.login(botconfig.token);
